57950cd8b1c1a6505d0bc46992f7f1bfb4cab014
ZeynepSchmid <77445806+ZeynepSchmid@users.noreply.github.com>
2021-02-17 16:58:24 +0100

    Update Client.cs

--- GameWebSiteSimulation/Client.cs
+++ GameWebSiteSimulation/Client.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace GameWebSiteSimulation
 {
     public class Client
@@ -15,80 +15,85 @@ namespace GameWebSiteSimulation
             setTCNo(tcno);
             setYear(year);
         }
-        public string getFirstName() {
-            return this.firstName;
-        }
-        public string getName()
-        {
-            return this.name;
-        }
-        public string getTCNo()
-        {
-            return this.TCNo;
-        }
-        public int getYear()
-        {
-            return this.year;
-        }
+      
         public int getAge() {
             return 2021 - this.year;
         }
 
-        public void setFirstName(String firstname)
+        public Boolean setFirstName(String firstname)
         {
             foreach (char letter in firstname) {
                 if (Char.IsDigit(letter)) {
                     Console.WriteLine("name can not contain numbers!");
-                    Environment.Exit(0);
+                    return false;
                 }
             }
 
             if (firstname.Length > 2)
                 this.firstName = firstname;
             else
+            {
                 Console.WriteLine("Name can not shorter than 2 letter!");
+                return false;
+            }
+
+            return true;
         }
 
-        public void setName(String name)
+        public Boolean setName(String name)
         {
             foreach (char letter in name)
             {
                 if (Char.IsDigit(letter))
                 {
                     Console.WriteLine("name can not contain numbers!");
-                    Environment.Exit(0);
+                    return false;
                 }
             }
 
             if (name.Length > 2)
                 this.name = name;
             else
+            {
                 Console.WriteLine("Name can not shorter than 2 letter!");
+                return false;
+            }
+            return true;
         }
 
-        public void setTCNo(string tcno)
+        public Boolean setTCNo(string tcno)
         {
             foreach (char number in tcno)
             {
                 if (!Char.IsDigit(number))
                 {
                     Console.WriteLine("TCNo can not contain letters!");
-                    Environment.Exit(0);
+                    return false;
                 }
             }
 
             if (tcno.Length == 11)
                 this.TCNo = tcno;
-            else
+            else {
                 Console.WriteLine("Please enter your 11-stage TCNo");
+                return false;
+            }
+            return true;
         }
 
-        public void setYear(int year)
+        public Boolean setYear(int year)
         {
             if (year >= 1900 && year <= 2021)
                 this.year = year;
             else
+            {
                 Console.WriteLine("Please enter a year in use");
+                return false;
+            }
+            return true;
+        }
+        public Boolean ifReal() {
+            return setFirstName(firstName) && setName(name) && setTCNo(TCNo) && setYear(year);    
         }
     }
 
